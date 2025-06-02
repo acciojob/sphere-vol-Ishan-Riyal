fda21b498876fd9413fb76f6e63999fccae7247b
Ishan Riyal <ishanriyal07@gmail.com>
2025-06-02 15:08:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 function volume_sphere() {
-    //Write your code here
-  
-} 
+	//Write your code 
+	const radiusInput = document.getElementById('radius').value;
+	const volumeOutput = document.getElementById('volume');
+
+	const r = parseFloat(radiusInput);
+
+	if (isNaN(r) || r < 0) {
+		volumeOutput.value = 'NaN';
+	    return false;
+	}
+
+	const volume = (4 / 3) * Math.PI * Math.pow(r, 3);
+	volumeOutput.value = volume.toFixed(4);
+	return false;
+}
+
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
